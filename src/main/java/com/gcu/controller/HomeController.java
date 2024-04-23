50399bc4cc2bbca88605de88488a8bb89aa92524
Zachary (Ace) Lake <96988100+AceLake@users.noreply.github.com>
2024-04-23 13:36:23 -0700

    testing pushing from cloud based IDE

--- src/main/java/com/gcu/controller/HomeController.java
+++ src/main/java/com/gcu/controller/HomeController.java
@@ -11,6 +11,7 @@ public class HomeController {
     public String home(Model model) {
         // You can add any model attributes if needed
         // For example: model.addAttribute("title", "Welcome to my website");
+        // Testing comments out
         
         System.out.println("Here");
         return "home"; // This assumes you have a "home.html" Thymeleaf template
